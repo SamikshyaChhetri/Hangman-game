7d483665a7e72f228051d26a39294f9bfccf3162
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-06-28 18:40:16 +0545

    Hangman game completed

--- script.js
+++ script.js
@@ -40,23 +40,44 @@ console.log(a);
 let displayString = "_".repeat(wordLength);
 
 displayValue.innerHTML = displayString;
+let count = 0;
 
 document.getElementById("submit").addEventListener("click", () => {
   const inputValue = document.getElementById("input").value;
-
   if (inputValue.length > 1) {
     alert("Cannot input more than 1 value");
   } else {
     a.forEach((letter, index) => {
-      if (letter === inputValue) {
-        // console.log(index);
-        const splitDisplay = displayString.split("");
-        splitDisplay[index] = inputValue;
-        const joinDisplay = splitDisplay.join("");
-        displayString = joinDisplay;
-        displayValue.innerHTML = joinDisplay;
-        console.log(joinDisplay);
+      if (a.includes(inputValue)) {
+        if (letter === inputValue) {
+          // console.log(index);
+          const splitDisplay = displayString.split("");
+          splitDisplay[index] = inputValue;
+          const joinDisplay = splitDisplay.join("");
+          displayString = joinDisplay;
+          displayValue.innerHTML = joinDisplay;
+          console.log(joinDisplay);
+        }
       }
     });
+    if (!a.includes(inputValue)) {
+      count++;
+      if (count == 1) {
+        document.getElementById("head").classList.remove("hidden");
+        inputValue.innerHTML = " ";
+      } else if (count == 2) {
+        document.getElementById("leftHand").classList.remove("hidden");
+      } else if (count == 3) {
+        document.getElementById("rightHand").classList.remove("hidden");
+      } else if (count == 4) {
+        document.getElementById("body").classList.remove("hidden");
+      } else if (count == 5) {
+        document.getElementById("leftleg").classList.remove("hidden");
+      } else if (count == 6) {
+        document.getElementById("rightleg").classList.remove("hidden");
+        displayValue.innerHTML = "You lose";
+      }
+      console.log(count);
+    }
   }
 });
